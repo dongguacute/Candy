5c21ee66eec4d1a76786821efc5c93c48ed96a2e
dongguacute <gudupao2022@gmail.com>
2026-04-11 17:11:03 +0800

    feat(config): enhance Next.js configuration with turbopack support
    Added turbopack configuration to the Next.js setup, specifying the root path for improved build performance. This change aims to optimize the development workflow.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  // @ts-ignore
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
